1b14a5047526ac7cf208d0b9aa34ff19416f668a
fatioazizi3399-boop <fatioazizi3399@gmail.com>
2026-07-04 10:58:57 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
+// ساخت ستاره ها
 
+const stars=document.querySelector(".stars");
+
+for(let i=0;i<120;i++){
+
+    const star=document.createElement("div");
+
+    star.className="star";
+
+    star.style.left=Math.random()*100+"%";
+
+    star.style.top=Math.random()*100+"%";
+
+    star.style.animationDelay=Math.random()*3+"s";
+
+    stars.appendChild(star);
+
+}
+
+
+
+// ساخت قلب ها
+
+const hearts=document.getElementById("hearts");
+
+function createHeart(){
+
+    const heart=document.createElement("div");
+
+    heart.className="heart";
+
+    heart.innerHTML="❤️";
+
+    heart.style.left=Math.random()*100+"vw";
+
+    heart.style.bottom="-30px";
+
+    heart.style.fontSize=(20+Math.random()*30)+"px";
+
+    heart.style.animationDuration=(4+Math.random()*4)+"s";
+
+    hearts.appendChild(heart);
+
+    setTimeout(()=>{
+
+        heart.remove();
+
+    },8000);
+
+}
+
+setInterval(createHeart,350);
+
+
+
+// دکمه
+
+const btn=document.getElementById("startBtn");
+
+btn.onclick=function(){
+
+    alert("این تازه شروعشه... ❤️");
+
+}
